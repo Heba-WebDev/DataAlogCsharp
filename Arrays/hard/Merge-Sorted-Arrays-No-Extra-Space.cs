a50256be132b6bbcdcebe4081eb55d38feedaa9d
Heba-Webdev23 <jtvonline@yahoo.com>
2025-04-30 14:24:11 +0300

    feat: merge sorted arrays without extra space with gap method

--- Arrays/hard/Merge-Sorted-Arrays-No-Extra-Space.cs
+++ Arrays/hard/Merge-Sorted-Arrays-No-Extra-Space.cs
@@ -52,5 +52,66 @@ Time Complexity: O(n + m) + O(n log n) + O(m log m)
 Space Complexity: O(1)
 */
 
-// Gap method approach -> soon
+// Gap method approach
 
+public static void MergeTwoSortedArraysGapMethod(int[] arr1, int[] arr2)
+{
+    int n = arr1.Length, m = arr2.Length;
+    int gap = (n + m + 1) / 2;
+
+    while (gap > 0)
+    {
+        int i = 0, j = gap;
+
+        while (j < (n + m))
+        {
+            int val1 = GetValue(arr1, arr2, i, n);
+            int val2 = GetValue(arr1, arr2, j, n);
+
+            if (val1 > val2)
+                Swap(arr1, arr2, i, j, n);
+
+            i++;
+            j++;
+        }
+
+        gap = (gap == 1) ? 0 : (gap + 1) / 2;
+    }
+
+    Console.WriteLine(string.Join(", ", arr1));
+    Console.WriteLine(string.Join(", ", arr2));
+}
+
+private static int GetValue(int[] arr1, int[] arr2, int index, int n)
+{
+    return (index < n) ? arr1[index] : arr2[index - n];
+}
+
+private static void Swap(int[] arr1, int[] arr2, int i, int j, int n)
+{
+    if (i < n && j < n)
+    {
+        // sort within arr1
+        int temp = arr1[i];
+        arr1[i] = arr1[j];
+        arr1[j] = temp;
+    }
+    else if (i < n && j >= n)
+    {
+        // swap between arr1 and arr2
+        int temp = arr1[i];
+        arr1[i] = arr2[j - n];
+        arr2[j - n] = temp;
+    }
+    else
+    {
+        // sort within arr2
+        int temp = arr2[i - n];
+        arr2[i - n] = arr2[j - n];
+        arr2[j - n] = temp;
+    }
+}
+/*
+Time Complexity: O((n + m) * log(n + m))
+Space Complexity: O(1)
+*/
