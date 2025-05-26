4af29a88817bcd8376196733c5b34d816db57c0f
Heba-Webdev23 <jtvonline@yahoo.com>
2025-05-26 12:40:14 +0300

    feat: lower bound

---
+++ Binary-Search/Lower-Bound.cs
@@ -0,0 +1,32 @@
+/*
+Given a sorted array of nums and an integer x, write a program to find the
+lower bound of x. The lower bound algorithm finds the first or the smallest
+index in a sorted array where the value at that index is greater than or equal
+to a given key i.e. x.
+
+If no such index is found, return the size of the array.
+*/
+
+public static int LowerBound(int[] arr, int x)
+{
+    int left = 0, right = arr.Length;
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] < x)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return left;
+}
+
+// Time Complexity --> O(log n)
+// Space Complexity --> O(1)
